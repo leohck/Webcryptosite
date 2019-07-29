df5c5d1e46e27a33168f9c2b70150ad820aa4395
Jonas <jonas_barros@outlook.com>
2019-07-29 14:43:07 -0300

    Pushing to heroku

--- crypto/views.py
+++ crypto/views.py
@@ -5,12 +5,25 @@ import json
 
 # Create your views here.
 def home(request):
-
-    # Grab crypto values
-    api_request = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
-    price = json.loads(api_request.content)
+    # Grab crypto Price Data
+    price_request = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,BCH,EOS,LTC,XLM,ADA,USDT,"
+        "TRX&tsyms=USD,EUR")
+    price = json.loads(price_request.content)
 
     # Grab cryto datas
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     news = json.loads(api_request.content)
     return render(request, 'home.html', context={'news': news, 'price': price})
+
+
+def prices(request):
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        quote = quote.upper()
+        price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+quote+"&tsyms=USD")
+        price = json.loads(price_request.content)
+        return render(request, 'prices.html', {'price': price})
+    else:
+        notfound = "Enter a crypto value on the search"
+        return render(request, 'prices.html', {'notfound': notfound})
